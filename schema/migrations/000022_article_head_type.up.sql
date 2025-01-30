2d9b24a376edd3a8a3920b606c1fb71063d412e0
Kappa <kappa@entropi.kr>
2025-01-30 12:44:54 +0900

    feat: fix broken migrations for article_head_type

--- schema/migrations/000022_article_head_type.up.sql
+++ schema/migrations/000022_article_head_type.up.sql
@@ -1,4 +1,9 @@
-CREATE TABLE article_head_type (
-    id SERIAL PRIMARY KEY,
+CREATE TABLE article_head_type
+(
+    id   SERIAL PRIMARY KEY,
     name TEXT NOT NULL
 );
+
+alter table articles
+    add constraint fk_article_head
+        foreign key (head) references article_head_type (id);
